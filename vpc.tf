e3d36c278964200d475afad86e73365a6072bd83
venkateshtammali <39961342+venkateshtammali@users.noreply.github.com>
2019-10-18 14:32:42 +0530

    changes with variables

--- vpc.tf
+++ vpc.tf
@@ -5,9 +5,9 @@ terraform {
   backend "remote" {}
 }
 provider "aws" {
-  region  = "ap-south-1"
-  access_key = "accesskey"
-  secret_key = "secretkey"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION
 }
 }
 # Internet VPC
