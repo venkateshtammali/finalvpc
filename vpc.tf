9f934af23664988962833f11a90b677ded8f545b
venkateshtammali <39961342+venkateshtammali@users.noreply.github.com>
2019-11-14 00:07:32 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -23,7 +23,7 @@ resource "aws_vpc" "main" {
 
 # Subnets
 resource "aws_subnet" "main-public-1" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "${aws_vpc.main.id}"
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-3"
@@ -34,7 +34,7 @@ resource "aws_subnet" "main-public-1" {
 }
 
 resource "aws_subnet" "main-public-2" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "${aws_vpc.main.id}"
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-3"
@@ -45,7 +45,7 @@ resource "aws_subnet" "main-public-2" {
 }
 
 resource "aws_subnet" "main-private-1" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "${aws_vpc.main.id}"
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-3"
@@ -56,7 +56,7 @@ resource "aws_subnet" "main-private-1" {
 }
 
 resource "aws_subnet" "main-private-2" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = "${aws_vpc.main.id}"
   cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-3"
