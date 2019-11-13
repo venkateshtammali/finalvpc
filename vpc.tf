d962f7bf9fcc050f520169deb2a010ec3431c340
venkateshtammali <39961342+venkateshtammali@users.noreply.github.com>
2019-11-14 00:51:33 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -27,7 +27,7 @@ resource "aws_subnet" "main-public-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-3"
+    availability_zone = "eu-west-3a"
 
     tags {
         Name = "main-public-1"
@@ -37,7 +37,7 @@ resource "aws_subnet" "main-public-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-3"
+    availability_zone = "eu-west-3b"
 
     tags {
         Name = "main-public-2"
@@ -47,7 +47,7 @@ resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-3"
+    availability_zone = "eu-west-3a"
 
     tags {
         Name = "main-private-1"
@@ -57,7 +57,7 @@ resource "aws_subnet" "main-private-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.5.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-3"
+    availability_zone = "eu-west-3b"
 
     tags {
         Name = "main-private-2"
