1a8a5597517feaf75c36693d76ff0af29ff775ef
venkateshtammali <39961342+venkateshtammali@users.noreply.github.com>
2019-11-14 00:31:15 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -27,7 +27,7 @@ resource "aws_subnet" "main-public-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1a"
+    availability_zone = "eu-west-3"
 
     tags {
         Name = "main-public-1"
@@ -37,27 +37,17 @@ resource "aws_subnet" "main-public-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1b"
+    availability_zone = "eu-west-3"
 
     tags {
         Name = "main-public-2"
-    }
-}
-resource "aws_subnet" "main-public-3" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.3.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1c"
-
-    tags {
-        Name = "main-public-3"
     }
 }
 resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1a"
+    availability_zone = "eu-west-3"
 
     tags {
         Name = "main-private-1"
@@ -67,20 +57,10 @@ resource "aws_subnet" "main-private-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.5.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1b"
+    availability_zone = "eu-west-3"
 
     tags {
         Name = "main-private-2"
-    }
-}
-resource "aws_subnet" "main-private-3" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.6.0/24"
-    map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1c"
-
-    tags {
-        Name = "main-private-3"
     }
 }
 
