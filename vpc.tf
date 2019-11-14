b4af94273344220e25ac24e12651b2fc1806da8c
venkateshtammali <39961342+venkateshtammali@users.noreply.github.com>
2019-11-14 12:19:44 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -27,7 +27,7 @@ resource "aws_subnet" "main-public-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-3a"
+    availability_zone = "us-east-1a"
 
     tags {
         Name = "main-public-1"
@@ -37,17 +37,27 @@ resource "aws_subnet" "main-public-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-3b"
+    availability_zone = "us-east-1b"
 
     tags {
         Name = "main-public-2"
+    }
+}
+resource "aws_subnet" "main-public-3" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.3.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1c"
+
+    tags {
+        Name = "main-public-3"
     }
 }
 resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-3a"
+    availability_zone = "us-east-1a"
 
     tags {
         Name = "main-private-1"
@@ -57,10 +67,20 @@ resource "aws_subnet" "main-private-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.5.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-3b"
+    availability_zone = "us-east-1b"
 
     tags {
         Name = "main-private-2"
+    }
+}
+resource "aws_subnet" "main-private-3" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.6.0/24"
+    map_public_ip_on_launch = "false"
+    availability_zone = "us-east-1c"
+
+    tags {
+        Name = "main-private-3"
     }
 }
 
@@ -95,6 +115,10 @@ resource "aws_route_table_association" "main-public-2-a" {
     subnet_id = "${aws_subnet.main-public-2.id}"
     route_table_id = "${aws_route_table.main-public.id}"
 }
+resource "aws_route_table_association" "main-public-3-a" {
+    subnet_id = "${aws_subnet.main-public-3.id}"
+    route_table_id = "${aws_route_table.main-public.id}"
+}
 # nat gw
 resource "aws_eip" "nat" {
   vpc      = true
@@ -124,5 +148,9 @@ resource "aws_route_table_association" "main-private-1-a" {
 }
 resource "aws_route_table_association" "main-private-2-a" {
     subnet_id = "${aws_subnet.main-private-2.id}"
+    route_table_id = "${aws_route_table.main-private.id}"
+}
+resource "aws_route_table_association" "main-private-3-a" {
+    subnet_id = "${aws_subnet.main-private-3.id}"
     route_table_id = "${aws_route_table.main-private.id}"
 }
